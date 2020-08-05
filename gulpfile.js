224112f7bccc1ad5a85b23ea0267cdfa52bde2da
Андрей <techpcb@mail.ru>
2020-08-05 15:29:35 +0400

    Добавлены плагины работы с sass-файлами, добавлен autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 const { src, dest } = require("gulp");
 const gulp = require("gulp");
 
-const output = "out";
+const output = "dist";
 
 const ttf2woff2 = require("gulp-ttf2woff2");
 const ttf2woff = require("gulp-ttf2woff");
 const ttf2eot = require("gulp-ttf2eot");
 const browsersync = require("browser-sync").create();
+const del = require('del');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 function browserSync() {
   browsersync.init({
@@ -30,17 +33,44 @@ gulp.task("fonts", function () {
 
 function html() {
   return src("#src/*.html")
-    .pipe(dest("out"))
+    .pipe(dest(output))
     .pipe(browsersync.stream());
+}
+
+function css() {
+  return src("#src/sass/style.sass")
+    .pipe(sass(
+      {
+        outputStyle: "expanded"        
+      }
+    ))
+    .pipe(autoprefixer({
+      cascade: true,
+      overrideBrowserslist: ["last 5 version"]
+    }))
+    .pipe(dest(output+"/css/"))
+    .pipe(browsersync.stream());
+}
+
+function norm() {
+  return src("node_modules/normalize.css/normalize.css")
+    .pipe(dest(output+"/css/"));
+}
+
+function clean() {
+  return del("./" + output + "/");
 }
 
 function watchFiles(params) {
   gulp.watch(["#src/*.html"], html);
+  gulp.watch(["#src/sass/**/*.sass"], css);
 }
 
-const out = gulp.series(html);
+const out = gulp.series(clean, gulp.parallel(norm, css, html));
 const watch = gulp.parallel(out, watchFiles, browserSync);
 
+exports.norm = norm;
+exports.css = css;
 exports.html = html;
 exports.out = out;
 exports.watch = watch;
